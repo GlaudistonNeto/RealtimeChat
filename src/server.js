4e7d5f160712157bf8d6c6f3014d1129f5a01055
GlaudistonNeto <glaudistonson@gmail.com>
2021-12-05 22:42:17 -0300

    backend finalized

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const express  = require('express');
+const cors  = require('cors');
+const Pusher = require("pusher");
+
+const pusher = new Pusher({
+  appId: "1312185",
+  key: "068133b23cfaf634458b",
+  secret: "3f8506aa40993bedbf63",
+  cluster: "us3",
+  useTLS: true
+});
+
+const app = express();
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://localhost:8080']
+}))
+
+app.use(express.json());
+
+app.post('/api/messages', async (req, res) => {
+  await pusher.trigger("chat", "message", {
+    username: req.body.username,
+    message: req.body.message
+  });
+
+  res.json([]);
+});
+
+app.listen(8000)
+console.log('linstening to port 8000');
